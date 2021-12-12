feeffeb3a52bc893a0090304608a9ae7505cd56b
ArvsssDT <82487903+ArvsssDT@users.noreply.github.com>
2021-12-12 19:44:43 +0800

    Update until deleteMapping

--- src/main/java/com/csa/samplefullstack/controller/EmployeeController.java
+++ src/main/java/com/csa/samplefullstack/controller/EmployeeController.java
@@ -4,6 +4,7 @@ import com.csa.samplefullstack.entity.Employee;
 import com.csa.samplefullstack.repository.EmployeeRepository;
 import org.springframework.beans.factory.annotation.*;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.http.*;
 import java.util.List;
 
 @RestController
@@ -14,9 +15,30 @@ public class EmployeeController {
     @Autowired  // spring - dependency injection
     private EmployeeRepository employeeRepository;
     
-    @RequestMapping("/employees")
+    @GetMapping("/employees")
     public List<Employee> getAllEmployees(){
         return employeeRepository.findAll();
     }
+    
+    @PostMapping("/employees")
+    public Employee postEmployee(@RequestBody Employee employee){
+        return employeeRepository.save(employee);
+    }
+    
+    @GetMapping("/employees/{id}")
+    public Employee getEmployeeById(@PathVariable Long id){
+        return employeeRepository.findById(id).get();
+    }
+    
+    @PutMapping("/employees")
+     public Employee putEmployee(@RequestBody Employee employee){
+         return employeeRepository.save(employee);
+    }
+     
+    @DeleteMapping("/employees/{id}")
+    public ResponseEntity<HttpStatus> deleteEmployeeById(@PathVariable Long id){
+        employeeRepository.deleteById(id);
+        return new ResponseEntity<HttpStatus>(HttpStatus.NO_CONTENT);
+    }
+    
 }
-
--- src/main/java/com/csa/samplefullstack/entity/Employee.java
+++ src/main/java/com/csa/samplefullstack/entity/Employee.java
@@ -15,23 +15,23 @@ public class Employee{
     
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private int employee_id;
+    private Long employee_id;
     private String name;
     private String roles;
     private String nickname;
 
-    public Employee(int employee_id, String name, String roles, String nickname) {
+    public Employee(Long employee_id, String name, String roles, String nickname) {
         this.employee_id = employee_id;
         this.name = name;
         this.roles = roles;
         this.nickname = nickname;
     }
 
-    public int getEmployee_id() {
+    public Long getEmployee_id() {
         return employee_id;
     }
 
-    public void setEmployee_id(int employee_id) {
+    public void setEmployee_id(Long employee_id) {
         this.employee_id = employee_id;
     }
 
